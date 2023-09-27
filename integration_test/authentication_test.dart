2916a2d80114c1b69501404f94cbec870a91800a
ddiazghub <dedemoya@uninorte.edu.co>
2023-09-27 10:43:01 -0500

    Activity done

--- integration_test/authentication_test.dart
+++ integration_test/authentication_test.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+Future<void> fillForm(WidgetTester tester, String form, String email, String password) async {
+  final emailField = find.byKey(Key("TextFormField${form}Email"));
+  final passwordField = find.byKey(Key("TextFormField${form}Password"));
+
+  await tester.enterText(emailField, email);
+  await tester.enterText(passwordField, password);
+
+  await tester.pump();
+  await tester.tap(find.byKey(Key("Button${form}Submit")));
+  await tester.pumpAndSettle();
+}
+
+void findText(String text) {
+  final errorMessage = find.text(text);
+  expect(errorMessage, findsOneWidget);
+}
+
 void main() {
   Future<Widget> createHomeScreen() async {
     WidgetsFlutterBinding.ensureInitialized();
@@ -14,31 +31,78 @@ void main() {
   testWidgets("Login sin creación de usuario", (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    await fillForm(tester, "Login", "user@email.com", "12345678");
+    findText("User or passwor nok");
   });
 
-  testWidgets("Login -> signup -> creación usuario -> login no exitoso",
-      (WidgetTester tester) async {
+  testWidgets("Login -> signup -> creación usuario -> login no exitoso", (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    await tester.tap(find.byKey(const Key("ButtonLoginCreateAccount")));
+    await fillForm(tester, "SignUp", "user@email.com", "12345678");
+    await fillForm(tester, "Login", "user@email.com", "1234567");
+    findText("User or passwor nok");
   });
 
-  testWidgets("Login -> signup -> creación usuario -> login exitoso -> logout",
-      (WidgetTester tester) async {
+  testWidgets("Login -> signup -> creación usuario -> login exitoso -> logout", (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    const email = "user@email.com";
+    await tester.tap(find.byKey(const Key("ButtonLoginCreateAccount")));
+    await fillForm(tester, "SignUp", email, "12345678");
+    await fillForm(tester, "Login", email, "12345678");
+
+    findText("Hello $email");
+
+    final logout = find.byKey(const Key("ButtonHomeLogOff"));
+    await tester.tap(logout);
+    await tester.pumpAndSettle();
+
+    findText("Login with email");
   });
 
-  testWidgets(
-      "Login -> signup -> creación usuario -> login éxitoso -> logout -> login exitoso",
-      (WidgetTester tester) async {
+  testWidgets("Login -> signup -> creación usuario -> login éxitoso -> logout -> login exitoso", (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    const email = "user@email.com";
+    await tester.tap(find.byKey(const Key("ButtonLoginCreateAccount")));
+    await fillForm(tester, "SignUp", email, "12345678");
+    await fillForm(tester, "Login", email, "12345678");
+
+    findText("Hello $email");
+
+    final logout = find.byKey(const Key("ButtonHomeLogOff"));
+    await tester.tap(logout);
+    await tester.pumpAndSettle();
+
+    findText("Login with email");
+
+    await fillForm(tester, "Login", email, "12345678");
+    findText("Hello $email");
   });
 
-  testWidgets(
-      "Login -> signup -> creación usuario -> login éxitoso -> logout -> login no exitoso",
-      (WidgetTester tester) async {
+  testWidgets("Login -> signup -> creación usuario -> login éxitoso -> logout -> login no exitoso", (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    const email = "user@email.com";
+    await tester.tap(find.byKey(const Key("ButtonLoginCreateAccount")));
+    await fillForm(tester, "SignUp", email, "12345678");
+    await fillForm(tester, "Login", email, "12345678");
+
+    findText("Hello $email");
+
+    final logout = find.byKey(const Key("ButtonHomeLogOff"));
+    await tester.tap(logout);
+    await tester.pumpAndSettle();
+
+    findText("Login with email");
+
+    await fillForm(tester, "Login", email, "1234567");
+    findText("User or passwor nok");
   });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,44 +5,130 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:f_testing_template/main.dart';
+import 'package:f_testing_template/ui/pages/authentication/login.dart';
+import 'package:f_testing_template/ui/pages/content/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+
+void testForm(WidgetTester tester, String form, String email, String password, void Function() callback) async {
+  const app = MyApp();
+  await tester.pumpWidget(app);
+
+  final emailField = find.byKey(Key("TextFormField${form}Email"));
+  final passwordField = find.byKey(Key("TextFormField${form}Password"));
+
+  await tester.enterText(emailField, email);
+  await tester.enterText(passwordField, password);
+
+  await tester.pump();
+  await tester.tap(find.byKey(Key("Button${form}Submit")));
+  await tester.pumpAndSettle();
+
+  callback();
+}
+
+void findText(String text) {
+  final errorMessage = find.text(text);
+  expect(errorMessage, findsOneWidget);
+}
+
+void testLogin(WidgetTester tester, String email, String password, String error) async {
+  testForm(tester, "Login", email, password, () => findText(error));
+}
+
+void testSignUp(WidgetTester tester, String email, String password, String error) async {
+  testForm(tester, "Login", email, password, () => findText(error));
+}
 
 void main() {
-  testWidgets(
-      'Widget login validación @ email', (WidgetTester tester) async {});
+  testWidgets('Widget login validación @ email', (WidgetTester tester) async {
+    testLogin(tester, "invalid", "password", "Enter valid email address");
+  });
 
-  testWidgets('Widget login validación campo vacio email',
-      (WidgetTester tester) async {});
+  testWidgets('Widget login validación campo vacio email', (WidgetTester tester) async {
+    testLogin(tester, "", "password", "Enter email");
+  });
 
-  testWidgets('Widget login validación número de caracteres password',
-      (WidgetTester tester) async {});
+  testWidgets('Widget login validación número de caracteres password', (WidgetTester tester) async {
+    testLogin(tester, "user@email.com", "pass", "Password should have at least 6 characters");
+  });
 
-  testWidgets('Widget login validación campo vacio password',
-      (WidgetTester tester) async {});
+  testWidgets('Widget login validación campo vacio password', (WidgetTester tester) async {
+    testLogin(tester, "user@email.com", "", "Enter password");
+  });
 
-  testWidgets(
-      'Widget login autenticación exitosa', (WidgetTester tester) async {});
+  testWidgets('Widget login autenticación exitosa', (WidgetTester tester) async {
+  });
 
-  testWidgets(
-      'Widget login autenticación no exitosa', (WidgetTester tester) async {});
+  testWidgets('Widget login autenticación no exitosa', (WidgetTester tester) async {
+    testLogin(tester, "user@email.com", "12345678", "User or passwor nok");
+  });
 
-  testWidgets(
-      'Widget signUp validación @ email', (WidgetTester tester) async {});
+  testWidgets('Widget signUp validación @ email', (WidgetTester tester) async {
+    const email = "user@email.com";
+    const password = "12345678";
+    const app = GetMaterialApp(home: LoginScreen(email: email, password: password));
+    await tester.pumpWidget(app);
 
-  testWidgets('Widget signUp validación campo vacio email',
-      (WidgetTester tester) async {});
+    final emailField = find.byKey(const Key("TextFormFieldLoginEmail"));
+    final passwordField = find.byKey(const Key("TextFormFieldLoginPassword"));
 
-  testWidgets('Widget signUp validación número de caracteres password',
-      (WidgetTester tester) async {});
+    await tester.enterText(emailField, email);
+    await tester.enterText(passwordField, password);
 
-  testWidgets('Widget signUp validación campo vacio password',
-      (WidgetTester tester) async {});
+    await tester.pump();
+    await tester.tap(find.byKey(const Key("ButtonLoginSubmit")));
+    await tester.pumpAndSettle();
 
-  testWidgets(
-      'Widget home visualización correo', (WidgetTester tester) async {});
+    findText("Hello $email");
+  });
 
-  testWidgets('Widget home nevegación detalle', (WidgetTester tester) async {});
+  testWidgets('Widget signUp validación campo vacio email', (WidgetTester tester) async {
+    testSignUp(tester, "", "password", "Enter email");
+  });
 
-  testWidgets('Widget home logout', (WidgetTester tester) async {});
+  testWidgets('Widget signUp validación número de caracteres password', (WidgetTester tester) async {
+    testSignUp(tester, "user@email.com", "pass", "Password should have at least 6 characters");
+  });
+
+  testWidgets('Widget signUp validación campo vacio password', (WidgetTester tester) async {
+    testSignUp(tester, "user@email.com", "", "Enter password");
+  });
+
+  testWidgets('Widget home visualización correo', (WidgetTester tester) async {
+    const email = "user@email.com";
+    const password = "12345678";
+    const app = GetMaterialApp(home: HomePage(loggedEmail: email, loggedPassword: password));
+    await tester.pumpWidget(app);
+
+    findText("Hello $email");
+  });
+
+  testWidgets('Widget home nevegación detalle', (WidgetTester tester) async {
+    const email = "user@email.com";
+    const password = "12345678";
+    const app = GetMaterialApp(home: HomePage(loggedEmail: email, loggedPassword: password));
+    await tester.pumpWidget(app);
+
+    final detailsButton = find.byKey(const Key("ButtonHomeDetail"));
+    await tester.tap(detailsButton);
+    await tester.pumpAndSettle();
+
+    findText("Some detail");
+  });
+
+  testWidgets('Widget home logout', (WidgetTester tester) async {
+    const email = "user@email.com";
+    const password = "12345678";
+    const app = GetMaterialApp(home: HomePage(loggedEmail: email, loggedPassword: password));
+    await tester.pumpWidget(app);
+
+    final logout = find.byKey(const Key("ButtonHomeLogOff"));
+    await tester.tap(logout);
+    await tester.pumpAndSettle();
+
+    findText("Login with email");
+  });
 }
